a49dfeb737d47390d3209b3a975b8b695beb6f1e
Phatchara Champathong <phatchara.champathong@elev.ga.ntig.se>
2024-09-12 10:13:36 +0200

    ImDone

--- BashAmonster/obj/Debug/net8.0/BashAmonster.AssemblyInfo.cs
+++ BashAmonster/obj/Debug/net8.0/BashAmonster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BashAmonster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf07f359e375feba7782ae6fb0f6629ff056692e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b8ffb7780a7c1961997b408a5525a949b37793")]
 [assembly: System.Reflection.AssemblyProductAttribute("BashAmonster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BashAmonster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
